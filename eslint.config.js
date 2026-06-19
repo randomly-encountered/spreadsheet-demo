76da246411c5b4860ec94a58ff3578b9a29760fa
Stephen Seator <steve.seator@gmail.com>
2026-06-19 07:11:27 -0400

    include additional prettier/lint rules for good hygiene

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import eslintConfigPrettier from 'eslint-config-prettier'
+import perfectionist from 'eslint-plugin-perfectionist'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -10,12 +11,30 @@ export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: {
+      perfectionist
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite
     ],
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_'
+        }
+      ],
+      'perfectionist/sort-interfaces': ['error', { order: 'asc', type: 'alphabetical' }],
+      'perfectionist/sort-jsx-props': ['error', { order: 'asc', type: 'alphabetical' }],
+      'perfectionist/sort-object-types': ['error', { order: 'asc', type: 'alphabetical' }],
+      'perfectionist/sort-objects': ['error', { order: 'asc', type: 'alphabetical' }]
+    },
     languageOptions: {
       globals: globals.browser
     }
